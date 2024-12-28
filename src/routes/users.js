54209ba28a95ce2c1779296c20c2fd4ae03560cb
Nabaraj Rai <nabarajrai@Nabarajs-MacBook-Air.local>
2024-12-28 20:32:31 +0545

    feat: implement updateUserDetails API for user data modification

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getUser, getSuggestions } from "../controller/users.js";
+import {
+  getUser,
+  getSuggestions,
+  updateUserDetails,
+} from "../controller/users.js";
 
 const router = express.Router();
 
 router.get("/find/:userId", getUser);
 router.get("/suggestions", getSuggestions);
+router.put("/updateUserDetails", updateUserDetails);
 
 export default router;
